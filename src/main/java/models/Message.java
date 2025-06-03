87c19f177b32b257a550d5766945269187521351
User <User@SYSMHS3028>
2025-06-03 11:59:57 +0900

    Index Fuckin'hell

--- src/main/java/models/Message.java
+++ src/main/java/models/Message.java
@@ -2,14 +2,22 @@ package models;
 
 import java.sql.Timestamp;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllMessages",
+        query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+    )
+})
 @Table(name = "messages")
 public class Message {
     @Id
--- src/main/java/utils/DBUtil.java
+++ src/main/java/utils/DBUtil.java
@@ -1,8 +1,8 @@
 package utils;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 public class DBUtil {
     private static final String PERSISTENCE_UNIT_NAME = "message_board";
